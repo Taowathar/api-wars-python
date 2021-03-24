50ce9b0b925e2cd3f76a4b5e4684a666d6804298
Taowathar <taowathar@gmail.com>
2021-03-24 12:49:04 +0100

    Create base structure

--- server.py
+++ server.py
@@ -1,1 +1,12 @@
-# put your code here
+from flask import Flask, render_template
+
+app = Flask(__name__)
+
+
+@app.route('/')
+def main():
+    return render_template('index.html')
+
+
+if __name__ == '__main__':
+    app.run()
