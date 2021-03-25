a66cd9d13c0a9968784f102eb6be5f6b79d6b3d4
Taowathar <taowathar@gmail.com>
2021-03-25 15:37:39 +0100

    Implement registration function

--- data_manager.py
+++ data_manager.py
@@ -1,2 +1,10 @@
 import connection
 from psycopg2.extras import RealDictCursor
+
+
+@connection.connection_handler
+def add_new_user(cursor: RealDictCursor, user) -> list:
+    query = """
+        INSERT INTO users (username, password)
+        VALUES (%(username)s, %(password)s)"""
+    cursor.execute(query, user)
--- server.py
+++ server.py
@@ -1,4 +1,4 @@
-from flask import Flask, render_template, request, session
+from flask import Flask, render_template, request, redirect, session
 import os
 import util
 import data_manager
@@ -13,8 +13,17 @@ def main():
     return render_template('index.html')
 
 
-@app.route('/registration')
+@app.route('/registration', methods=['GET', 'POST'])
 def registration():
+    if request.method == 'POST':
+        if request.form['password'] and request.form['password']:
+            password = util.hash_password(request.form['password'])
+            user = {'username': request.form['password'], 'password': password}
+            data_manager.add_new_user(user)
+            return redirect('/')
+        else:
+            error = 'Please, fill in both fields.'
+            return render_template('registration.html', error=error)
     return render_template('registration.html')
     
     
